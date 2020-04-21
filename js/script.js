738163f7410587352e7eca2486a05322795e7e7f
Jack B. Du <jd2804@nyu.edu>
2020-04-21 19:38:21 +0400

    ported to google sheets api

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ var app = new Vue({
       window.scrollTo(0, window.innerWidth*.48);
     },
     onSearchEnter: function(el) {
-      console.log(el.children[0].focus());
+      el.children[0].focus();
     }
   },
   computed: {
@@ -168,17 +168,131 @@ var app = new Vue({
   }
 })
 
-function init() {
-  Tabletop.init( {
-    key: 'https://docs.google.com/spreadsheets/d/19seYcWtzkf4YZTyaobE63dm8B9k0_9dqv0RZ4l5Vo-Q/edit?usp=sharing',
-    simpleSheet: false }
-  ).then(function(data, tabletop) {
-    console.log(data);
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '872432590149-9njmctf5n0dqarh2d8b53suggj50rnvg.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDMDqqNf2u8kLYubDhJ_rNYV1Qai_FWZPU';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = 'none';
+    signoutButton.style.display = 'block';
+    listMajors();
+  } else {
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+  }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+function listMajors() {
+  const sheetNames = ["Projects", "Videos", "'Live Events'", "'Recorded Events'", "'Archival Book'"];
+
+  gapi.client.sheets.spreadsheets.values.batchGet({
+    spreadsheetId: '149I1zwGZUTgrriM0lS5FgHSP_Px2wSlLftMDQN6vt6g',
+    ranges: sheetNames,
+  }).then(function(response) {
+    var data = new Object();
+    for (var k = 0; k < response.result.valueRanges.length; k++) {
+      var range = response.result.valueRanges[k];
+      if (range.values.length > 0) {
+        let columnNames = [];
+        var sheetData = [];
+        for (let i = 0; i < range.values[0].length; i++) {
+          columnNames.push(range.values[0][i]);
+        }
+        for (let i = 1; i < range.values.length; i++) {
+          let rowData = new Object();
+          for (let j = 0; j < range.values[i].length; j++) {
+            rowData[columnNames[j]] = range.values[i][j];
+          }
+          sheetData.push(rowData);
+        }
+        for (sheetName of sheetNames) {
+          if (range.range.includes(sheetName)) {
+              data[sheetName] = sheetData;
+          }
+        }
+      }
+    }
+
     let tiles = [];
 
     // loading projects
     let majors = [];
-    projectsData = data["Projects"].elements;
+    let projectsData = data[sheetNames[0]];
     for (let i = 0; i < projectsData.length; i++) {
       tiles.push(new Tile('project',
         projectsData[i]['Title'] ? projectsData[i]['Title'] : "",
@@ -194,7 +308,7 @@ function init() {
     app.majors = majors;
 
     // loading live events
-    liveEventsData = data["Live Events"].elements;
+    let liveEventsData = data[sheetNames[2]];
     for (let i = 0; i < liveEventsData.length; i++) {
       tiles.push(new Tile('live',
         liveEventsData[i]['Title'] ? liveEventsData[i]['Title'] : '',
@@ -206,7 +320,7 @@ function init() {
     }
 
     // loading recorded events
-    recordedEventsData = data["Recorded Events"].elements;
+    let recordedEventsData = data[sheetNames[3]];
     for (let i = 0; i < recordedEventsData.length; i++) {
       tiles.push(new Tile('recorded',
         recordedEventsData[i]['Title'] ? recordedEventsData[i]['Title'] : '',
@@ -218,7 +332,7 @@ function init() {
     }
 
     // loading videos
-    videoData = data["Videos"].elements;
+    let videoData = data[sheetNames[1]];
     for (let i = 0; i < videoData.length; i++) {
       tiles.push(new Tile('video',
         videoData[i]['Title'] ? videoData[i]['Title'] : '',
@@ -230,7 +344,7 @@ function init() {
     }
 
     // loading archival book
-    bookData = data["Archival Book"].elements;
+    let bookData = data[sheetNames[4]];
     for (let i = 0; i < bookData.length; i++) {
       tiles.push(new Tile('book',
         bookData[i]['Title'] ? bookData[i]['Title'] : '',
@@ -244,7 +358,8 @@ function init() {
     app.tiles = tiles;
     app.tilesFilteredByTag = tiles;
     window.scrollTo(0,0);
-    console.log('top');
-  })
+
+  }, function(response) {
+    console.log('Error: ' + response.result.error.message);
+  });
 }
-window.addEventListener('DOMContentLoaded', init)
