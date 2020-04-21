76703a6065804c480906530cc160d9df7668064f
Jack B. Du <jd2804@nyu.edu>
2020-04-22 00:39:11 +0400

    redirect google api response to console.log

--- js/script.js
+++ js/script.js
@@ -248,9 +248,7 @@ function handleSignoutClick(event) {
  * @param {string} message Text to be placed in pre element.
  */
 function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
+  console.log("Google API: "+message);
 }
 
 /**
