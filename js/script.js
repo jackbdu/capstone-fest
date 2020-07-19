a923529bea61f2b78ec96cc0723813526970742c
Jack B. Du <jd2804@nyu.edu>
2020-07-20 02:11:44 +0400

    updated with sample data for demo purposes

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ var app = new Vue({
     tilesFilteredByTag: [],
     majors: [],
     activeTag: '',
-    isSignedIn: null
+    isSignedIn: true
   },
   methods: {
     filterTilesByTag: function(selectedTag) {
@@ -179,99 +179,6 @@ var app = new Vue({
   }
 })
 
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '872432590149-9njmctf5n0dqarh2d8b53suggj50rnvg.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyDMDqqNf2u8kLYubDhJ_rNYV1Qai_FWZPU';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/userinfo.profile";
-
-var authorizeButton = document.getElementById('login');
-var signoutButton = document.getElementById('logout');
-var loginPage = document.getElementById('signin-page');
-var mainPage = document.getElementById('app');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  }, function(error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    app.isSignedIn = true;
-    loadData();
-  } else {
-    app.isSignedIn = false;
-    removeData();
-  }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-  console.log("Google API: "+message);
-}
-
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-
-function removeData() {
-  app.tiles = [];
-  app.tilesFilteredByTag = [];
-  app.majors = []
-}
-
 function updateCountdown(tile) {
   var goal = new Date(tile.subtitle);
   var timezone = 4;
@@ -304,93 +211,97 @@ function millis2time(timeInMillis) {
   return hours + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
 }
 
-function loadData() {
-  Tabletop.init( {
-    key: 'https://docs.google.com/spreadsheets/d/1x0_wAVJ86WC1WAQOYweKGUDwWJuMe9q0P7zHRvbpVRA/edit?usp=sharing',
-    simpleSheet: false }
-  ).then(function(data, tabletop) {
+function parseData(data) {
+  const sheetNames = ["Projects", "Videos", "Live Events", "Recorded Events", "Archival Book"];
+  let tiles = [];
 
-    const sheetNames = ["Projects", "Videos", "Live Events", "Recorded Events", "Archival Book"];
-    let tiles = [];
-
-    // loading projects
-    let majors = [];
-    let projectsData = data[sheetNames[0]].elements;
-    for (let i = 0; i < projectsData.length; i++) {
-      tiles.push(new Tile('project',
-        projectsData[i]['Order'] ? projectsData[i]['Order'] : "",
-        projectsData[i]['Title'] ? projectsData[i]['Title'] : "",
-        projectsData[i]['Preferred Name'] ? projectsData[i]['Preferred Name'] : (projectsData[i]['First Name'] ? projectsData[i]['First Name'] + " ": "")+(projectsData[i]['Last Name'] ? projectsData[i]['Last Name'] : ""),
-        projectsData[i]['Description'] ? projectsData[i]['Description'] : "",
-        projectsData[i]['Link'] ? projectsData[i]['Link'] : "",
-        projectsData[i]['Image Link'] ? projectsData[i]['Image Link'] : "",
-        projectsData[i]['Tags'] ? projectsData[i]['Tags'].split(', ') : []));
-      if (!majors.some(major => major.name === projectsData[i]['Major'])) {
-        majors.push({ name: projectsData[i]['Major'] });
-      }
+  // loading projects
+  let majors = [];
+  let projectsData = data[sheetNames[0]];
+  for (let i = 0; i < projectsData.length; i++) {
+    tiles.push(new Tile('project',
+      projectsData[i]['Order'] ? projectsData[i]['Order'] : "",
+      projectsData[i]['Title'] ? projectsData[i]['Title'] : "",
+      projectsData[i]['Preferred Name'] ? projectsData[i]['Preferred Name'] : (projectsData[i]['First Name'] ? projectsData[i]['First Name'] + " ": "")+(projectsData[i]['Last Name'] ? projectsData[i]['Last Name'] : ""),
+      projectsData[i]['Description'] ? projectsData[i]['Description'] : "",
+      projectsData[i]['Link'] ? projectsData[i]['Link'] : "",
+      projectsData[i]['Image Link'] ? projectsData[i]['Image Link'] : "",
+      projectsData[i]['Tags'] ? projectsData[i]['Tags'].split(', ') : []));
+    if (!majors.some(major => major.name === projectsData[i]['Major'])) {
+      majors.push({ name: projectsData[i]['Major'] });
     }
-    app.majors = majors;
+  }
+  app.majors = majors;
 
-    // loading live events
-    let liveEventsData = data[sheetNames[2]].elements;
-    for (let i = 0; i < liveEventsData.length; i++) {
-      tiles.push(new Tile('live',
-        liveEventsData[i]['Order'] ? liveEventsData[i]['Order'] : '',
-        liveEventsData[i]['Title'] ? liveEventsData[i]['Title'] : '',
-        liveEventsData[i]['Time'] ? liveEventsData[i]['Time'] : '',
-        liveEventsData[i]['Description'] ? liveEventsData[i]['Description'] : '',
-        liveEventsData[i]['Link'] ? liveEventsData[i]['Link'] : '',
-        liveEventsData[i]['Duration'] ? liveEventsData[i]['Duration'] : '',
-        liveEventsData[i]['Tags'] ? liveEventsData[i]['Tags'].split(', ') : []));
+  // loading live events
+  let liveEventsData = data[sheetNames[2]];
+  for (let i = 0; i < liveEventsData.length; i++) {
+    tiles.push(new Tile('live',
+      liveEventsData[i]['Order'] ? liveEventsData[i]['Order'] : '',
+      liveEventsData[i]['Title'] ? liveEventsData[i]['Title'] : '',
+      liveEventsData[i]['Time'] ? liveEventsData[i]['Time'] : '',
+      liveEventsData[i]['Description'] ? liveEventsData[i]['Description'] : '',
+      liveEventsData[i]['Link'] ? liveEventsData[i]['Link'] : '',
+      liveEventsData[i]['Duration'] ? liveEventsData[i]['Duration'] : '',
+      liveEventsData[i]['Tags'] ? liveEventsData[i]['Tags'].split(', ') : []));
+  }
+
+  // loading recorded events
+  let recordedEventsData = data[sheetNames[3]];
+  for (let i = 0; i < recordedEventsData.length; i++) {
+    tiles.push(new Tile('recorded',
+      recordedEventsData[i]['Order'] ? recordedEventsData[i]['Order'] : '',
+      recordedEventsData[i]['Title'] ? recordedEventsData[i]['Title'] : '',
+      recordedEventsData[i]['Time'] ? recordedEventsData[i]['Time'] : '',
+      recordedEventsData[i]['Description'] ? recordedEventsData[i]['Description'] : '',
+      recordedEventsData[i]['Link'] ? recordedEventsData[i]['Link'] : '',
+      '',
+      recordedEventsData[i]['Tags'] ? recordedEventsData[i]['Tags'].split(', ') : []));
+  }
+
+  // loading videos
+  let videoData = data[sheetNames[1]];
+  for (let i = 0; i < videoData.length; i++) {
+    tiles.push(new Tile('video',
+      videoData[i]['Order'] ? videoData[i]['Order'] : '',
+      videoData[i]['Title'] ? videoData[i]['Title'] : '',
+      '',
+      videoData[i]['Description'] ? videoData[i]['Description'] : '',
+      videoData[i]['Video Link'] ? videoData[i]['Video Link'] : '',
+      videoData[i]['Cover Link'] ? videoData[i]['Cover Link'] : '',
+      videoData[i]['Tags'] ? videoData[i]['Tags'].split(', ') : []));
+  }
+
+  // loading archival book
+  let bookData = data[sheetNames[4]];
+  for (let i = 0; i < bookData.length; i++) {
+    tiles.push(new Tile('book',
+      bookData[i]['Order'] ? bookData[i]['Order'] : '',
+      bookData[i]['Title'] ? bookData[i]['Title'] : '',
+      '',
+      '',
+      bookData[i]['Book Link'] ? bookData[i]['Book Link'] : '',
+      bookData[i]['Image Link'] ? bookData[i]['Image Link'] : '',
+      bookData[i]['Tags'] ? bookData[i]['Tags'].split(', ') : []));
+  }
+
+  app.tiles = tiles;
+  app.tilesFilteredByTag = tiles;
+  window.scrollTo(0,0);
+
+  for (tile of app.tiles) {
+    if (tile.type === "live") {
+      updateCountdown(tile);
     }
-
-    // loading recorded events
-    let recordedEventsData = data[sheetNames[3]].elements;
-    for (let i = 0; i < recordedEventsData.length; i++) {
-      tiles.push(new Tile('recorded',
-        recordedEventsData[i]['Order'] ? recordedEventsData[i]['Order'] : '',
-        recordedEventsData[i]['Title'] ? recordedEventsData[i]['Title'] : '',
-        recordedEventsData[i]['Time'] ? recordedEventsData[i]['Time'] : '',
-        recordedEventsData[i]['Description'] ? recordedEventsData[i]['Description'] : '',
-        recordedEventsData[i]['Link'] ? recordedEventsData[i]['Link'] : '',
-        '',
-        recordedEventsData[i]['Tags'] ? recordedEventsData[i]['Tags'].split(', ') : []));
-    }
-
-    // loading videos
-    let videoData = data[sheetNames[1]].elements;
-    for (let i = 0; i < videoData.length; i++) {
-      tiles.push(new Tile('video',
-        videoData[i]['Order'] ? videoData[i]['Order'] : '',
-        videoData[i]['Title'] ? videoData[i]['Title'] : '',
-        '',
-        videoData[i]['Description'] ? videoData[i]['Description'] : '',
-        videoData[i]['Video Link'] ? videoData[i]['Video Link'] : '',
-        videoData[i]['Cover Link'] ? videoData[i]['Cover Link'] : '',
-        videoData[i]['Tags'] ? videoData[i]['Tags'].split(', ') : []));
-    }
-
-    // loading archival book
-    let bookData = data[sheetNames[4]].elements;
-    for (let i = 0; i < bookData.length; i++) {
-      tiles.push(new Tile('book',
-        bookData[i]['Order'] ? bookData[i]['Order'] : '',
-        bookData[i]['Title'] ? bookData[i]['Title'] : '',
-        '',
-        '',
-        bookData[i]['Book Link'] ? bookData[i]['Book Link'] : '',
-        bookData[i]['Image Link'] ? bookData[i]['Image Link'] : '',
-        bookData[i]['Tags'] ? bookData[i]['Tags'].split(', ') : []));
-    }
-
-    app.tiles = tiles;
-    app.tilesFilteredByTag = tiles;
-    window.scrollTo(0,0);
-
-    for (tile of app.tiles) {
-      if (tile.type === "live") {
-        updateCountdown(tile);
-      }
-    }
-  })
+  }
 }
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    parseData(JSON.parse(this.responseText));
+  }
+};
+
+xhttp.open("GET", "sample-data/sample-database.json", true);
+xhttp.send();
